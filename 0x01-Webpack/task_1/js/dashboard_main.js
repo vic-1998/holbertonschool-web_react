d6173e60818e89377e28f93cd3d033ea066b1798
victor.plaza <victor.plaza@tekbees.com>
2022-05-12 18:16:31 -0500

    Typescript and Webpack

--- 0x01-Webpack/task_1/js/dashboard_main.js
+++ 0x01-Webpack/task_1/js/dashboard_main.js
@@ -1,23 +1,16 @@
-import $ from 'jquery';
+import $ from "jquery";
 import _ from 'lodash';
 
+$('body').append('<p>Holberton Dashboard</p>');
+$('body').append('<p>Dashboard data for the students</p>');
+$('body').append('<button>Click here to get started</button>');
+$('body').append(`<p id='count'></p>`);
+$('body').append('<p>Copyright - Holberton School</p>');
+
+$('button').on('click', _.debounce(updateCounter, 500));
+
 let count = 0;
-
 function updateCounter() {
-	count += 1;
-	return count;
+  count += 1;
+  $('#count').html(`${count} clicks on the button`);
 }
-
-$(function() {
-	$('body').append('<p>Holberton Dashboard</p>');
-	$('body').append('<p>Dashboard data for the students</p>');
-	$('body').append('<button>Click here to get started</button>');
-	$('body').append("<p id='count'></p>");
-	$('body').append('<p>Copyright - Holberton School</p>');
-
-	let debouncedFunc = _.debounce(() => {
-		let count = updateCounter();
-		$('#count').text(`${count} clicks on the button`);
-	});
-	$('button').on('click', debouncedFunc);
-});
